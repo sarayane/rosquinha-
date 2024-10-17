c0d391c52564a54e0f000405c5a5d787e8c6028c
sarayane <sarayane.paulino@escola.pr.gov.br>
2024-10-17 11:46:38 -0300

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const donut =
+document.getelementbyid('donut');
+
+function.resetanimation() {
+    donut.style.animation = 'none';
+    donut.offsetjeight;
+    donut.style.animation = '30';
+}
+
+setinterval(resetanimation, 4000);
